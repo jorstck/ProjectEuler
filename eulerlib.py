e099d6b613ea2c4a749ac4e76d40d1938cf984c5
jorstck <jorstack@gmail.com>
2019-10-22 22:05:17 -0400

    documented library

--- eulerlib.py
+++ eulerlib.py
@@ -7,15 +7,21 @@ Created by Jordan Stack
 from fractions import Fraction
 
 
-# Checks if a number is a perfect square
 def is_square(x: int) -> bool:
+    '''
+    x: number that is being checked to see if its a perfect square
+    returns true if x is a perfect square, false if not
+    '''
     if x < 0:
         return False
     return int(x**.5)**2 == x
 
 
-# Checks if a number is prime
 def is_prime(x: int) -> bool:
+    '''
+    x: number whose primality is being checked
+    returns true if x is prime, false if not
+    '''
     if x <= 1:
         return False
     elif x <= 3:
@@ -29,38 +35,58 @@ def is_prime(x: int) -> bool:
         return True
 
 
-# Returns a list of the prime factors of a number
 def list_prime_factors(number: int) -> list:
+    '''
+    number: an integer
+    returns a list of prime factors for the given number
+    '''
     list_factors = []
     while number != 1 or number != 1:
-        for x in range(2, int(num**.5)):
+        for x in range(2, int(number**.5)):
             if number % x == 0 and is_prime(x):
                 number /= x
                 list_factors.append(x)
     return(list_factors)
 
 
-# Returns sum of the digits of a number. Ex:    1234 -> 1 + 2 + 3 + 4 = 10
 def digital_sum(number: int) -> int:
+    '''
+    number: an integer
+    returns a sum of the digits of the given number
+    Ex: 1234 -> 1 + 2 + 3 + 4 = 10
+
+    '''
     return sum([int(s) for s in str(number)])
 
 
-# Returns the number of digits of a number. Ex: 1 -> 1, 11 -> 2, 111 -> 3
 def num_digits(number: int) -> int:
+    '''
+    number: an integer
+    returns the number of digits in the number
+    Ex: 1 -> 1, 11 -> 2, 111 -> 3
+    '''
     return len(str(number))
 
 
-# Returns true if the numerator of a number has more digits than denominator. Ex: 123/12 -> True, 1/12 -> False, 3/4 -> False
 def more_digits_numerator(number: float) -> bool:
+    '''
+    number: the number in question
+    returns true if number of digits in numerator is larger than number of digits in denominator
+    Ex: 123/12 -> True, 1/12 -> False, 3/4 -> False
+    '''
     frac = Fraction(number).limit_denominator()
-    numLength = len(str(frac.numerator))
-    denLength = len(str(frac.denominator))
+    numLength = num_digits(frac.numerator)
+    denLength = num_digits(frac.denominator)
     return numLength > denLength
 
 
-# Returns true if the denominator of a number has more digits than numerator. Ex: 123/12 -> False, 1/12 -> True, 3/4 -> False
 def more_digits_denomator(number: float) -> bool:
+    '''
+    number: the number in question
+    returns true if number of digits in denominator is larger than number of digits in numerator
+    Ex: 123/12 -> False, 1/12 -> True, 3/4 -> False
+    '''
     frac = Fraction(number).limit_denominator()
-    numLength = len(str(frac.numerator))
-    denLength = len(str(frac.denominator))
+    numLength = num_digits(frac.numerator)
+    denLength = num_digits(frac.denominator)
     return numLength < denLength
